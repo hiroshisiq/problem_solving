a4814ebcc04747fbacd3edd5b7607f9d22acdf46
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-04-02 13:29:21 -0300

    feat(quiz4): add challange 1523 submission

---
+++ quiz4/1523-estacionamento-linear.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple
+
+# Car is a tuple with arrival and departure time
+Car = Tuple[int, int]
+
+
+def read_line() -> Tuple[int, int]:
+    x, y = [int(i) for i in input().split(' ')]
+    return (x, y)
+
+
+def read_all() -> List[Tuple[int, int]]:
+    line = read_line()
+    lines = []
+
+    while line[0] and line[1]:
+        lines.append(line)
+        for _ in range(line[0]):
+            lines.append(read_line())
+        line = read_line()
+
+    return lines
+
+
+def run_test_case(
+    number_of_cars: int,
+    parking_lot_size: int,
+    cars: List[Car]
+) -> bool:
+    # Initialize parking_lot
+    parking_lot: List[Car] = []
+
+    # Run test
+    for _ in range(number_of_cars):
+        car = cars.pop(0)
+
+        # Remove cars while last car leaved before car arrival
+        while parking_lot and parking_lot[-1][1] <= car[0]:
+            parking_lot.pop()
+
+        # Check if car departure is before last car departure
+        if parking_lot and parking_lot[-1][1] < car[1]:
+            return False
+
+        # Park new car
+        parking_lot.append(car)
+
+        # Check size
+        if len(parking_lot) > parking_lot_size:
+            return False
+
+    # All cars were parked succesfully
+    return True
+
+
+if __name__ == "__main__":
+    info = read_all()
+
+    while info:
+        n, k = info.pop(0)
+        cars = info[:n]
+        del info[:n]
+
+        if run_test_case(number_of_cars=n, parking_lot_size=k, cars=cars):
+            print('Sim')
+        else:
+            print('Nao')
---
+++ quiz4/readme.md
@@ -0,0 +1,9 @@
+
+## Quiz 04
+
+This section includes challanges about stacks, queues and circular lists.
+
+## Content
+The challanges included here can all be found at https://www.urionlinejudge.com.br/
+
+- [1523](https://www.urionlinejudge.com.br/judge/pt/problems/view/1523) Estacionamento Linear;
