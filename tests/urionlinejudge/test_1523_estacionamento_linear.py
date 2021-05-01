c1463c1bff5b8bb611f22e5601df4b43732cf53a
Anderson Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-05-01 15:21:48 -0300

    test(uri): add pytest support (#1)
    * refactor(project): restructure folders
    
    * test(uri): add pytest
    
    * refactor(test): rename module
    
    * refactor(test): rename module
    
    * chore(project): add requirements.txt

---
+++ tests/urionlinejudge/test_1523_estacionamento_linear.py
@@ -0,0 +1,16 @@
+import pytest
+
+from tests.run_script import run_script, read_text
+
+RESOURCES_PATH = './tests/urionlinejudge/resources/1523/'
+EXECUTABLE_PATH = './problems/urionlinejudge/1523-estacionamento-linear.py'
+
+@pytest.mark.parametrize(
+    'input_path, output_path',
+    [(f'{RESOURCES_PATH}/case-1.in', f'{RESOURCES_PATH}/case-1.out')]
+)
+def test_1523_estacionamento_linear(input_path: str, output_path: str):
+    got = run_script(EXECUTABLE_PATH, input_path)
+    expected = read_text(output_path)
+
+    assert got == expected
