7114b7e33ce73f608daa3753bc36d01fd1d35a35
Hiroshi Siqueira <anderson.siqueira@loggi.com>
2021-05-01 21:37:05 -0300

    feat(unicamp): solution for 2016p1f1

---
+++ tests/unicamp/test_2016p1f1_tacos_de_bilhar.py
@@ -0,0 +1,18 @@
+import pytest
+
+from tests.run_script import run_script, read_text
+
+RESOURCES_PATH = './tests/unicamp/resources/2016p1f1'
+EXECUTABLE_PATH = './problems/unicamp/2016p1f1_tacos_de_bilhar.py'
+
+
+@pytest.mark.parametrize(
+    'input_path, output_path',
+    [(f'{RESOURCES_PATH}/case_1.in', f'{RESOURCES_PATH}/case_1.out'),
+     (f'{RESOURCES_PATH}/case_2.in', f'{RESOURCES_PATH}/case_2.out')]
+)
+def test_2016p1f1_tacos_de_bilhar(input_path: str, output_path: str):
+    got = run_script(EXECUTABLE_PATH, input_path)
+    expected = read_text(output_path)
+
+    assert got == expected
