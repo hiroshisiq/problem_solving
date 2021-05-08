02522f0a8b2c8902afa4a45e42e21a9c7fc6f196
Hiroshi Siqueira <anderson.siqueira@loggi.com>
2021-05-07 22:57:27 -0300

    refactor(uni): changes to comply to lint

--- problems/unicamp/uni_2009p1f2_olimpiadas.py
+++ problems/unicamp/uni_2009p1f2_olimpiadas.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from typing import Tuple, List
 
-class Country():
-    def __init__(self, id: int):
-        self.id = id
+
+class Country:
+    def __init__(self, country_id: int):
+        self.id = country_id
         self.gold_medals = 0
         self.silver_medals = 0
         self.bronze_medals = 0
@@ -11,31 +12,35 @@ class Country():
 
 def read_medal_winners() -> Tuple[int, int, int]:
     g, s, b = [int(i) for i in input().split(' ')]
-    return (g, s, b)
+    return g, s, b
+
 
 def read_countries_and_categories() -> Tuple[int, int]:
     m, n = [int(i) for i in input().split(' ')]
-    return (m, n)
+    return m, n
+
 
 def sort_countries_by_medal_count(countries: List[Country]) -> List[Country]:
-    sorted_countries = sorted(countries, key=lambda country: country.bronze_medals, reverse=True)
-    sorted_countries = sorted(sorted_countries, key=lambda country: country.silver_medals, reverse=True)
-    sorted_countries = sorted(sorted_countries, key=lambda country: country.gold_medals, reverse=True)
-    return sorted_countries
+    s_countries = sorted(countries, key=lambda country: country.bronze_medals, reverse=True)
+    s_countries = sorted(s_countries, key=lambda country: country.silver_medals, reverse=True)
+    s_countries = sorted(s_countries, key=lambda country: country.gold_medals, reverse=True)
+    return s_countries
+
 
 if __name__ == "__main__":
-    m, n = read_countries_and_categories()
-    countries = [Country(id=id+1) for id in range(m)]
+    number_of_countries, number_of_sports = read_countries_and_categories()
+    country_list = [Country(country_id=country_id + 1)
+                    for country_id in range(number_of_countries)]
 
-    for _ in range(n):
+    for _ in range(number_of_sports):
         gold_winner, silver_winner, bronze_winner = read_medal_winners()
 
         # Update medal counter
-        countries[gold_winner-1].gold_medals += 1
-        countries[silver_winner-1].silver_medals += 1
-        countries[bronze_winner-1].bronze_medals += 1
+        country_list[gold_winner - 1].gold_medals += 1
+        country_list[silver_winner - 1].silver_medals += 1
+        country_list[bronze_winner - 1].bronze_medals += 1
 
-    sorted_countries = sort_countries_by_medal_count(countries)
+    sorted_countries = sort_countries_by_medal_count(country_list)
     result = ' '.join([str(country.id) for country in sorted_countries])
 
     print(result)
--- problems/unicamp/uni_2016p1f1_tacos_de_bilhar.py
+++ problems/unicamp/uni_2016p1f1_tacos_de_bilhar.py
@@ -1,25 +1,27 @@
-from collections import namedtuple
+#!/usr/bin/env python3
 
-class BilliardCue():
-    def __init__(self, lenght):
-        self.length = lenght
+class BilliardCue:
+    def __init__(self, length):
+        self.length = length
         self.stock_quantity = 0
         self.total = 0
 
-def getOrFabricateCue(cue: BilliardCue):
+
+def get_or_fabricate_cue(cue: BilliardCue):
     if cue.stock_quantity == 0:
         cue.stock_quantity += 1
         cue.total += 2
     else:
         cue.stock_quantity -= 1
 
+
 if __name__ == "__main__":
     quantity = int(input())
 
     store = {}
-    for lenght in input().split(' '):
-        cue = store.setdefault(lenght, BilliardCue(lenght))
-        getOrFabricateCue(cue)
+    for billiard_length in input().split(' '):
+        billiard_cue = store.setdefault(billiard_length, BilliardCue(billiard_length))
+        get_or_fabricate_cue(billiard_cue)
 
     result = sum([cue.total for cue in store.values()])
     print(result)
