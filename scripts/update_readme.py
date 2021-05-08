28bf0cd24f799f0b9ac2cda0209741f6c314f6b5
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-05-08 13:29:56 -0300

    chore(project): add readme

---
+++ scripts/update_readme.py
@@ -0,0 +1,27 @@
+import os
+
+PATH = '../problems'
+content = ''
+
+# Open base file
+with open('base_readme.md', 'r') as f:
+    content += f.read()
+
+# Get all readme.md files inside PATH and append its content
+for path, dirs, files in os.walk(PATH):
+    readme_list = [filename for filename in files if 'readme.md' in filename]
+
+    if not readme_list:
+        continue
+
+    full_path_list = [os.path.join(path, readme) for readme in readme_list]
+    for full_path in full_path_list:
+        with open(full_path, 'r') as f:
+            content += f.read()
+
+# Write final readme to root directory
+with open('../readme.md', 'w+') as f:
+    f.write(content)
+
+
+
