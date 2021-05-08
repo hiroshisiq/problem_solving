33fb864d54bdae6ba68639478ffdf2f4f03d15c2
Hiroshi Siqueira <anderson.siqueira@loggi.com>
2021-05-07 21:24:39 -0300

    feat(uri): solution for 1194

--- tests/urionlinejudge/test_1194_prefix_infix_postfix.py
+++ tests/urionlinejudge/test_1194_prefix_infix_postfix.py
@@ -1,13 +1,15 @@
 import pytest
+from problems.urionlinejudge.uri_1194_prefix_infix_postfix import get_postorder
 
 from tests.run_script import run_script, read_text
 
 RESOURCES_PATH = './tests/urionlinejudge/resources/1194'
-EXECUTABLE_PATH = './problems/urionlinejudge/1194_prefix_infix_postfix.py'
+EXECUTABLE_PATH = './problems/urionlinejudge/uri_1194_prefix_infix_postfix.py'
 
 @pytest.mark.parametrize(
     'input_path, output_path',
-    [(f'{RESOURCES_PATH}/case_1.in', f'{RESOURCES_PATH}/case_1.out')]
+    [(f'{RESOURCES_PATH}/case_1.in', f'{RESOURCES_PATH}/case_1.out'),
+     (f'{RESOURCES_PATH}/case_2.in', f'{RESOURCES_PATH}/case_2.out')]
 )
 def test_1194_prefix_infix_postfix(input_path: str, output_path: str):
     got = run_script(EXECUTABLE_PATH, input_path)
