1e8c39df3a24d1d656e67e85a804d80b7b000a37
Hiroshi Siqueira <anderson.siqueira@loggi.com>
2021-05-07 22:32:56 -0300

    feat(cf): add initial solution

--- problems/codeforces/cf_1490_d_permutation_transformation.py
+++ problems/codeforces/cf_1490_d_permutation_transformation.py
@@ -1,2 +1,25 @@
+#!/usr/bin/env python3
+from typing import Tuple, List
+
+
+def read_test_case() -> Tuple[str, List[str]]:
+    array_size = input()
+    test_array = input().split(' ')
+    return array_size, test_array
+
+
+def array_to_depth(array: List[str], size: int, depth: int) -> str:
+    index = array.index(max(array))
+
+    left = array_to_depth(array[:index], index, depth+1) if index != 0 else ''
+    right = array_to_depth(array[index+1:], size-index-1, depth+1) if index != size-1 else ''
+
+    return f'{left} {depth} {right}'.strip()
+
+
 if __name__ == "__main__":
-    print('Not implemented!')
+    number_of_test_cases = int(input())
+    for _ in range(number_of_test_cases):
+        size, array = read_test_case()
+        result = array_to_depth(array, int(size), 0)
+        print(result)
