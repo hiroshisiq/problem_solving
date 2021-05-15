93c4b4315e2d97dc1a34592d8840f7911f176241
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-05-15 17:54:31 -0300

    feat(uri): initial solution for 1076

--- problems/urionlinejudge/uri_1076_drawing_mazes.py
+++ problems/urionlinejudge/uri_1076_drawing_mazes.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 
 
+# DFS algorithm
+from typing import Dict, Set
+
+Graph = Dict[str, Set[str]]
+
+
+def depth_first_search(graph: Graph, initial_vertex: str, visited: Graph, counter: int = 0):
+    visited.add(initial_vertex)
+
+    for next_vertex in graph[initial_vertex] - visited:
+        counter = depth_first_search(graph, next_vertex, visited, counter)
+        counter += 2
+
+    return counter
+
+
 def solve_problem():
-    print('Not implemented')
+    number_of_cases = int(input())
+
+    for _ in range(number_of_cases):
+        initial_vetex = int(input())
+        number_of_vertices, number_of_edges = [int(i) for i in input().split()]
+
+        # Initialize graph (incidence list representation)
+        graph = dict((vertex, set()) for vertex in range(number_of_vertices))
+
+        # Populate graph
+        for entry in range(number_of_edges):
+            vertex_a, vertex_b = [int(i) for i in input().split()]
+            graph[vertex_a].add(vertex_b)
+            graph[vertex_b].add(vertex_a)
+
+        counter = depth_first_search(graph, initial_vetex, set())
+        print(counter)
 
 
 if __name__ == "__main__":
